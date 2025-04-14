a77cb03ea3c7d416ed18f2229963222715cfb01f
andriesvdw1 <andriesvdw1@gmail.com>
2025-04-14 13:12:07 +0200

    Add button component, add Form and InputBox

---
+++ react-app/src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <input type='button' value='add task'/>
+  )
+}
+
+export default Button
---
+++ react-app/src/components/Form/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../Button/Button'
+import InputBox from '../InputBox/InputBox'
+function Form() {
+  return (
+    <form>
+        <InputBox/>
+        <Button/>
+    </form>
+  )
+}
+
+export default Form
